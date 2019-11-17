f630f483809e56890def90ae6cb679af33c9b636
themsou <themsou.ldv@gmail.com>
2019-11-17 12:18:09 +0100

    Setup node.js

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
+console.log('Bonjour le fichier index.js à bien été lançé !');
+
+// Configure logger settings
+var logger = require('winston');
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// -END
+
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+var auth = require('./auth.json');
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if(msg.content === 'ping'){
+
     msg.reply('Pong!');
+
+    msg.react('👍').then(() => msg.react('👎'));
+
+    const filter = (reaction, user) => {
+    	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === msg.author.id;
+    };
+
+    msg.awaitReactions(filter, { max: 1, time: 5000, errors: ['time'] }).then(collected => {
+
+    		const reaction = collected.first();
+    		if(reaction.emoji.name === '👍'){
+    			msg.reply('Moi aussi, j\'approuve');
+    		}else{
+    			msg.reply(';(');
+    		}
+
+    	}).catch(collected => {
+    		msg.reply('Hello !');
+    	});
   }
 });
-
-client.login('NTkwNTU0Nzc1NjEyMDMwOTc2.XXqBAw.lsmELPls984imiaNsAnKPdYnz70');
