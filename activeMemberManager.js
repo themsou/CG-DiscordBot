f6ede1d066d9e8478ce168ae70dd3c2715e4aa38
Clément GRENNERAT <clement.grennerat@free.fr>
2020-06-04 18:54:04 +0200

    Change ActiveGrades min messages
    Changement du nombre de messages minimum pour obtenir les grades 
    d'activité :
    @Apprenti  : 50 messages
    @Expert    : 100 messages
    @No-Life   : 200 messages
    @Cinéphile : 500 messages
    
    + Réglage d'un bug : les membres qui avaient un rôle avaient aussi tous 
    les rôles d'en dessous : un No-Life était aussi Expert et Apprenti.

--- activeMemberManager.js
+++ activeMemberManager.js
@@ -26,26 +26,31 @@ var day = function day(){
         totalMsgs += (msgs === undefined) ? 0 : msgs;
       }
 
-      if(totalMsgs >= 30){
-        member.addRole('712619370077225010');
-      }else{
-        member.removeRole('712619370077225010');
-      }
-      if(totalMsgs >= 60){
-        member.addRole('712620267037392916');
-      }else{
-        member.removeRole('712620267037392916');
-      }
-      if(totalMsgs >= 100){
-        member.addRole('712620584659451984');
-      }else{
-        member.removeRole('712620584659451984');
-      }
-      if(totalMsgs >= 200){
+      if(totalMsgs >= 500){
         member.addRole('712620923131396146');
       }else{
         member.removeRole('712620923131396146');
+        if(totalMsgs >= 200){
+          member.addRole('712620584659451984');
+        }else{
+          member.removeRole('712620584659451984');
+          if(totalMsgs >= 100){
+            member.addRole('712620267037392916');
+          }else{
+            member.removeRole('712620267037392916');
+            if(totalMsgs >= 50){
+              member.addRole('712619370077225010');
+            }else{
+              member.removeRole('712619370077225010');
+            }
+          }
+        }
       }
+
+
+
+
+
 
       // Set le nombre de messages envoyés aujourd'hui à 0
       let file = EditJsonFile('./members.json');
